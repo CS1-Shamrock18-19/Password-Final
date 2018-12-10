7655f15045d14ad8d66849ad1b77783900bcbc88
Mr. Fleming <efleming@stpatrick.org>
2018-12-10 12:41:13 -0600

    initial upload

---
+++ securepass.js
@@ -0,0 +1,92 @@
+//returns true or false if the length is at least 8 character long
+function lengthCheck(password){
+
+  if(password.length >= 8){
+    return true
+  }
+  else{
+    return false;
+  }
+
+}
+
+
+
+
+
+
+//returns true or false if there is an uppercase character in the password
+function upperCaseCheck(password){
+
+  for(/*here*/){
+
+    if(/*here*/){
+      return true;
+    }
+
+  }
+  return false;
+}
+
+
+
+
+
+
+//returns true or false if there is an lowercase character in the password
+function lowerCaseCheck(password){
+
+
+}
+
+
+
+
+
+
+
+
+//returns true or false if there is a numerical character
+function numberCheck(password){
+
+
+}
+
+
+
+
+
+
+
+//returns true or false if there is a special character in the password
+function specialCheck(check){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+function passedAllChecks(){
+
+}
+
+
+
+
+
+
+
+
+
+//returns a string which represents the grade for your password
+function grade(password){
+
+
+}
